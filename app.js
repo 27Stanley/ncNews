98635674d37789a30c1221645624dba53edf6097
Stanley <27.stanleylch@gmail.com>
2023-10-04 10:33:59 +0100

    6-get-article-comments

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express")
-const {getAllTopics, getAllEndpoints, getArticleById, getAllArticles} = require ("./controller/app.controller.js")
+const {
+    getAllTopics, 
+    getAllEndpoints, 
+    getArticleById, 
+    getAllArticles, 
+    getArticleCommentsById
+} = require ("./controller/app.controller.js")
 
 const app = express()
 
@@ -13,25 +19,28 @@ app.get(`/api/articles/:article_id`, getArticleById)
 
 app.get(`/api/articles`, getAllArticles)
 
+app.get(`/api/articles/:article_id/comments`, getArticleCommentsById)
+
 
 app.all("/*", (req, res, next) => {
     res.status(404).send({message: "path not found"})
 })
 
 app.use((err, req, res, next) => {
-    if (err.status === 400){
-        console.log(err)
+    if (err.status === 400 && err.message === "invalid id path"){
         res.status(400).send({message: "invalid id path"})
     }
-    if (err.status === 404){
-        console.log(err)
-        res.status(404).send({message: "article not found"})
-    }
-    next()
+    next(err)
 })
 
 app.use((err, req, res, next) => {
-    console.log(err, 'unhandled error here')
+    if (err.status === 404 && err.message === "article not found"){
+        res.status(404).send({message: "article not found"})
+    }
+    next(err)
+})
+
+app.use((err, req, res, next) => {
     res.status(500).send('Server Error!');
 })
 
