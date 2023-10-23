c46be416c2bfcb07a0408816552bb2250850283e
Stanley <27.stanleylch@gmail.com>
2023-10-23 11:49:07 +0100

    cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require('cors');
 
 const {
     getAllTopics, 
@@ -28,6 +29,8 @@ const {
 const app = express()
 
 app.use(express.json())
+
+app.use(cors());
 
 // app.get(`/api/healthCheck`, healthCheck)
 
